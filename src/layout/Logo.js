d568f06f185d6179487b5d3c0c9f1cf4ac7fdb87
Michał Skiba <skiba.kontakt@gmail.com>
2019-04-06 17:51:01 +0200

    create top menu

---
+++ src/layout/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import 'src/assets/styles/header.scss'
+
+export class Header extends React.Component {
+  render() {
+    return (
+      <h2 class="nav-logo">Stock Tracker</h2>
+    )
+  }
+}
