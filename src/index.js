a4b2247ec9cf7eadeb0d6eb6fd7bc1bd15261c9e
Michał Skiba <skiba.kontakt@gmail.com>
2019-04-06 17:04:37 +0200

    add reset css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'reset-css';
 import './assets/styles/main.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
