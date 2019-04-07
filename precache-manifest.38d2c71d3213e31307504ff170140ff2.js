b3b74f16029f72bedcff24c3a09f1541f6e69fb1
Michał Skiba <skiba.kontakt@gmail.com>
2019-04-07 21:58:11 +0200

    Updates

---
+++ precache-manifest.38d2c71d3213e31307504ff170140ff2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a29025819440690bca5",
+    "url": "/stock-tracer/static/js/runtime~main.88c5a741.js"
+  },
+  {
+    "revision": "eefe4ed0e07efe01c7cd",
+    "url": "/stock-tracer/static/js/main.f593b49a.chunk.js"
+  },
+  {
+    "revision": "702b67d234a4e5bdc80e",
+    "url": "/stock-tracer/static/js/2.0c41fa6c.chunk.js"
+  },
+  {
+    "revision": "eefe4ed0e07efe01c7cd",
+    "url": "/stock-tracer/static/css/main.e7c63b42.chunk.css"
+  },
+  {
+    "revision": "702b67d234a4e5bdc80e",
+    "url": "/stock-tracer/static/css/2.97d7bdbd.chunk.css"
+  },
+  {
+    "revision": "0d559a23b0f0d22047d50d6a14da6de4",
+    "url": "/stock-tracer/index.html"
+  }
+];
